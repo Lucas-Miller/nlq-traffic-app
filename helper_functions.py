1b16f66978428cb764f1cb3cce165350875356da
lucas <millerlucas25@gmail.com>
2021-03-31 01:27:08 -0400

    Working scann searcher, returns images

--- helper_functions.py
+++ helper_functions.py
@@ -11,19 +11,3 @@ def find_matches(image_embeddings, queries, k=9, normalize=True):
     results = tf.math.top_k(dot_similarity, k).indices.numpy()
     # Return matching image paths.
     return [[image_paths[idx] for idx in indices] for indices in results]
-
-def find_matches_scann(image_embeddings, query_embedding, k=9, normalize=True):
-    
-    # Normalize the query and the image embeddings.
-    if normalize:
-        image_embeddings = tf.math.l2_normalize(image_embeddings, axis=1)
-        #query_embedding = tf.math.l2_normalize(query_embedding, axis=1)
-    # Create ScaNN searcher
-    searcher = scann.scann_ops.builder(normalized_dataset, 10, "dot_product").tree(
-    num_leaves=2000, num_leaves_to_search=100, training_sample_size=250000).score_ah(
-    2, anisotropic_quantization_threshold=0.2).reorder(100).build()
-
-    # Use ScaNN to find top neighbors
-    results = searcher.search_batched(query_embedding)
-    # Return matching image paths.
-    return [[image_paths[idx] for idx in indices] for indices in results]
